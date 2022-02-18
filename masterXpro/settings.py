3eb8eabd4c03e4297904109318c1a70ae1a3b5af
mrxkhan57 <ibrakhimkhan58@gmail.com>
2022-02-18 12:57:03 +0500

    add cors

--- masterXpro/settings.py
+++ masterXpro/settings.py
@@ -51,7 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'masterXpro.urls'
 
